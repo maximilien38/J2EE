34ab5e7a593ea8c22225808c21b2984ad1b64eb1
maximilien <maximilien-faure@hotmail.fr>
2016-04-22 00:13:05 +0200

    adding text in servlet

--- test/src/test/servlets/MyFirstServelet.java
+++ test/src/test/servlets/MyFirstServelet.java
@@ -1,8 +1,28 @@
 package test.servlets;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class MyFirstServelet extends HttpServlet{
-	
+	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+		response.setContentType("text/html");
+		response.setCharacterEncoding( "UTF-8" );
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<meta charset=\"utf-8\" />");
+		out.println("<title>My first servlet</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<p>I'm the best in the world ! </p>");
+		out.println("</body>");
+		out.println("</html>");
+	}
 
 }
